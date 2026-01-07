934e3188021de1632bb2640e1b5ff5e054e17afc
IDCrisis0 <IDCrisis0@gmail.com>
2026-01-06 20:39:55 -0500

    Added functionality to read and print book to console.

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def get_book_text(path):
+    with open(path) as f:
+        book_text = f.read()
+        return book_text
+
+def main():
+    bookPath = "books/frankenstein.txt"
+    bookText = get_book_text(bookPath)
+
+    print(bookText)
+
+main()
