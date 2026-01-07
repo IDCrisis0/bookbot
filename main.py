36dd061880b2048424f959dc8f1d378abb9af112
IDCrisis0 <IDCrisis0@gmail.com>
2026-01-07 11:49:05 -0500

    Adding word count

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+def main():
+    bookPath = "books/frankenstein.txt"
+    bookText = get_book_text(bookPath)
+    wordCount = get_word_count(bookText)
+
+    print(bookText)
+    print(wordCount)
+
 def get_book_text(path):
     with open(path) as f:
         book_text = f.read()
         return book_text
 
-def main():
-    bookPath = "books/frankenstein.txt"
-    bookText = get_book_text(bookPath)
+def get_word_count(bookText):
+    word_count = bookText.split()
+    return len(word_count)
 
-    print(bookText)
-
-main()
+main() 
