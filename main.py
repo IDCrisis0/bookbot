f927ea2844625af1a8d49f0d869b23413da54ac4
IDCrisis0 <IDCrisis0@gmail.com>
2026-01-07 18:26:22 -0500

    Added functionality for character count and moved functions to stats.py -- Had to cheat to figure out how to do the dict to list workflow. RIP

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
-from stats import get_word_count
-from stats import get_character_count
+from stats import get_word_count, get_char_count, dict_to_sorted_list
 
 def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
     num_words = get_word_count(book_text)
-    num_characters = get_character_count(book_text)
+    num_chars = get_char_count(book_text)
+    chars_sorted = dict_to_sorted_list(num_chars)
+    book_report(book_path, num_words, chars_sorted)
 
-    print(book_text)
-    print(f"Found {num_words} total words")
-    print(num_characters)
-
+# get the text of a specified document.
+# book files kept in /books directory of program as .txt.
 def get_book_text(path):
     with open(path) as f:
         book_text = f.read()
         return book_text
+    
+def book_report(book_path, num_words, chars_sorted):
+    print ("============ BOOKBOT ============")
+    print (f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    for i in chars_sorted:
+        if i["char"].isalpha():
+            print(f"{i["char"]}: {i["num"]}")
+
 
 main() 
--- stats.py
+++ stats.py
@@ -1,13 +1,27 @@
+# gets the total word count of a specified text
 def get_word_count(text):
     word_count = text.split()
     return len(word_count)
 
-def get_character_count(text):
-    characters = {}
+# lowers all characters, and then counts the total of a specified text
+def get_char_count(text):
+    char = {}
     for i in text:
-        lower = i.lower()
-        if lower in characters:
-            characters[lower] += 1
+        lowered = i.lower()
+        if lowered in char:
+            char[lowered] += 1
         else:
-            characters[lower] = 1
-    return characters
+            char[lowered] = 1
+    return char
+
+# used to sort dictionary items by a specific value.
+def sort_on(items):
+    value = "num"
+    return items[value]
+
+def dict_to_sorted_list(num_chars):
+    sorted_list = []
+    for i in num_chars:
+        sorted_list.append({"char": i, "num": num_chars[i]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
